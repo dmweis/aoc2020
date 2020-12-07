1ab88c89d3ef10ffa38a8995fb1fc70352041496
David Weis <dweis7@gmail.com>
2020-12-07 20:38:57 +0000

    day 7 task 2 yaya

--- src/day_7.rs
+++ src/day_7.rs
@@ -17,11 +17,13 @@ fn input() -> &'static str {
 pub enum DayError {
     #[error("FailedParsingRule")]
     FailedParsingRule,
+    #[error("FailedTreeTraversal")]
+    FailedTreeTraversal,
 }
 
 const MY_BAG: &str = "shiny gold";
 
-#[derive(Debug, Eq, PartialEq)]
+#[derive(Debug, Eq, PartialEq, Clone)]
 struct ContainedBag {
     color: String,
     count: usize,
@@ -73,19 +75,25 @@ impl BagRule {
 #[derive(Debug, PartialEq, Eq)]
 struct RuleSet {
     bag_to_containers: HashMap<String, Vec<String>>,
+    bag_to_children: HashMap<String, Vec<ContainedBag>>,
 }
 
 impl RuleSet {
     fn parse_ruleset(input: &str) -> Result<RuleSet> {
         let rules = input.lines().map(BagRule::parse).filter_map(Result::ok);
         let mut bag_to_containers = HashMap::new();
+        let mut bag_to_children = HashMap::new();
         for rule in rules {
+            bag_to_children.insert(rule.parent.clone(), rule.children.clone());
             for child in rule.children {
                 let containers = bag_to_containers.entry(child.color).or_insert(vec![]);
                 containers.push(rule.parent.clone());
             }
         }
-        Ok(RuleSet { bag_to_containers })
+        Ok(RuleSet {
+            bag_to_containers,
+            bag_to_children,
+        })
     }
 
     fn count_topmost_containers(&self) -> usize {
@@ -104,6 +112,26 @@ impl RuleSet {
         // remove starting color
         top_most_colors.len() - 1
     }
+
+    fn count_contained(&self) -> Result<usize> {
+        let start = MY_BAG;
+        let mut rolling_count = 0;
+        let mut seen = VecDeque::new();
+        seen.push_front(start);
+        while let Some(node) = seen.pop_back() {
+            let children = self
+                .bag_to_children
+                .get(node)
+                .ok_or(DayError::FailedTreeTraversal)?;
+            rolling_count += 1;
+            for child in children {
+                for _ in 0..child.count {
+                    seen.push_front(&child.color);
+                }
+            }
+        }
+        Ok(rolling_count - 1)
+    }
 }
 
 fn task_1(input: &str) -> Result<usize> {
@@ -111,9 +139,14 @@ fn task_1(input: &str) -> Result<usize> {
     Ok(rule_set.count_topmost_containers())
 }
 
+fn task_2(input: &str) -> Result<usize> {
+    let rule_set = RuleSet::parse_ruleset(input)?;
+    Ok(rule_set.count_contained()?)
+}
+
 pub fn run() {
     println!("Day 7 task 1 -> {}", task_1(input()).unwrap());
-    // println!("Day 7 task 2 -> {}", task_2(input()));
+    println!("Day 7 task 2 -> {}", task_2(input()).unwrap());
 }
 
 #[cfg(test)]
@@ -121,7 +154,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn example_1() {
+    fn task_1_example_1() {
         let example = "light red bags contain 1 bright white bag, 2 muted yellow bags.
 dark orange bags contain 3 bright white bags, 4 muted yellow bags.
 bright white bags contain 1 shiny gold bag.
@@ -133,6 +166,34 @@ faded blue bags contain no other bags.
 dotted black bags contain no other bags.";
         let rule_set = RuleSet::parse_ruleset(example).unwrap();
         assert_eq!(rule_set.count_topmost_containers(), 4);
+    }
+
+    #[test]
+    fn task_2_example_1() {
+        let example = "light red bags contain 1 bright white bag, 2 muted yellow bags.
+dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+bright white bags contain 1 shiny gold bag.
+muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+faded blue bags contain no other bags.
+dotted black bags contain no other bags.";
+        let rule_set = RuleSet::parse_ruleset(example).unwrap();
+        assert_eq!(rule_set.count_contained().unwrap(), 32);
+    }
+
+    #[test]
+    fn task_2_example_2() {
+        let example = "shiny gold bags contain 2 dark red bags.
+dark red bags contain 2 dark orange bags.
+dark orange bags contain 2 dark yellow bags.
+dark yellow bags contain 2 dark green bags.
+dark green bags contain 2 dark blue bags.
+dark blue bags contain 2 dark violet bags.
+dark violet bags contain no other bags.";
+        let rule_set = RuleSet::parse_ruleset(example).unwrap();
+        assert_eq!(rule_set.count_contained().unwrap(), 126);
     }
 
     #[test]
@@ -198,6 +259,12 @@ dark orange bags contain 3 bright white bags, 4 muted yellow bags.";
         let res = task_1(input()).unwrap();
         assert!(res < 204);
         assert_eq!(res, 197);
+    }
+
+    #[test]
+    fn task_2_test() {
+        let res = task_2(input()).unwrap();
+        assert_eq!(res, 85324);
     }
 
     #[test]
