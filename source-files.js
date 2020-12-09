dab256c184ab1c672eb559dd28b2764b3b5914cb
dmweis <dmweis@users.noreply.github.com>
2020-12-09 21:16:58 +0000

    deploy: 0ac494127cd0d36fe546e1eb225b8dac753f59ec

--- source-files.js
+++ source-files.js
@@ -1,3 +1,3 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["aoc2020"] = {"name":"","files":["day_1.rs","day_2.rs","day_3.rs","day_4.rs","day_5.rs","day_6.rs","day_7.rs","day_8.rs","main.rs"]};
+sourcesIndex["aoc2020"] = {"name":"","files":["day_1.rs","day_2.rs","day_3.rs","day_4.rs","day_5.rs","day_6.rs","day_7.rs","day_8.rs","day_9.rs","main.rs"]};
 createSourceSidebar();
